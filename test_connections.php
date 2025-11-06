98aca098eeebf82f8e4e6324bb7700a2ba6f2054
unknown <charlesaeronbabas@gmail.com>
2025-11-06 21:32:31 +0800

    Save local changes before rebase

--- test_connections.php
+++ test_connections.php
@@ -1,13 +1,4 @@
 <?php
-$DB_HOST = 'sql100.infinityfree.com';
-$DB_USER = 'if0_40337753';
-$DB_PASS = 'lr3fB6jGpV';
-$DB_NAME = 'if0_40337753_matrixdb';
-
-$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
-
-if ($conn->connect_error) {
-    die("❌ Connection failed: " . $conn->connect_error);
-} 
-echo "✅ Database connected successfully!";
+require_once 'config.php';
+echo "✅ Successfully connected to the online FreeDB database!";
 ?>
